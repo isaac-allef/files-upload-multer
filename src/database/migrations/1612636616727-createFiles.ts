d79b84814b7a08bb772f16b9c1748a01a26a4e75
Isaac Allef <isaac_allef@hotmail.com>
2021-02-06 15:49:26 -0300

    added id column in 'createFiles' migration

--- src/database/migrations/1612636616727-createFiles.ts
+++ src/database/migrations/1612636616727-createFiles.ts
@@ -3,9 +3,17 @@ import {MigrationInterface, QueryRunner, Table} from "typeorm";
 export default class createFiles1612636127121 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
         await queryRunner.createTable( new Table({
             name: 'files',
             columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: true,
+                    generationStrategy: 'uuid',
+                    default: 'uuid_generate_v4()',
+                },
                 {
                     name: 'filename',
                     type: 'varchar',
